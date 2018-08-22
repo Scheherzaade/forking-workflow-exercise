63ed6a982572a89d03eaf0ac302b682c9ac12c17
Sebastian Bohr <sebastian.bohr1@rwth-aachen.de>
2018-08-22 11:20:34 +0000

    add group 99

--- group-99.py
+++ group-99.py
@@ -1,2 +1,2 @@
 def tweet():
-    return "rm -rf /"
+    return "\n _____________ \n < git is nice > \n ------------- \n   \ \n    \ \n        .--. \n       |o_o |\n       |:_/ |\n      //   \ \ \n     (|     | )\n    /'\_   _/`\ \n    \___)=(___/"
